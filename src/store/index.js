dc0405c90486f95819875be837756812468c90bd
南风北巷 <2498482388@qq.com>
2022-02-07 18:33:32 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 import { createStore } from 'vuex'
 
+// 创建vuex仓库并导出
 export default createStore({
   state: {
+    // 数据
   },
   mutations: {
+    // 改数据函数
   },
   actions: {
+    // 请求数据函数
   },
   modules: {
+  // 区分模块
+  },
+  getters: {
+    // vuex的计算属性
   }
 })
